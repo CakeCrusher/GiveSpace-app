ac6770d7b3cd9302f5de4ecbf99d2f2c764b779d
CakeCrusher <1sebastian1sosa1@gmail.com>
2021-12-05 21:48:13 -0500

    contacts to friends & friends

--- utils/schemas.js
+++ utils/schemas.js
@@ -52,3 +52,51 @@ mutation MyMutation($text: String = "") {
 //   "text": "Bob wants a bat to use his car with a bat for weels"
 // }
 
+export const ADD_CONTACTS_AS_FRIENDS = `
+mutation MyMutation($user_phone_number: String = "", $contacts_phone_numbers: [String!] = "") {
+  contacts_to_friends(contacts_phone_numbers: $contacts_phone_numbers, user_phone_number: $user_phone_number) {
+    new_friend_rels
+  }
+}
+`
+// {
+//   "user_phone_number": "+17865557297",
+//   "contacts_phone_numbers": ["+19545557297"]
+// }
+
+export const CREATE_FRIEND_REL = `
+mutation MyMutation($user_first: String! = "", $user_second: String! = "", $type: String = "friends") {
+  insert_friend_rel(objects: {user_second: $user_second, type: $type, user_first: $user_first}) {
+    returning {
+      id
+    }
+  }
+}
+`
+// {
+//   "user_first": "+17865557297",
+//   "user_second": "+19545557297",
+//   "type": "friends"
+// }
+
+export const FIND_FRIEND_RELS = `
+query MyQuery($id: [uuid!] = "") {
+  friend_rel(where: {id: {_in: $id}}) {
+    type
+    id
+    user {
+      phone_number
+      profile_pic_url
+      username
+    }
+    userByUserSecond {
+      phone_number
+      profile_pic_url
+      username
+    }
+  }
+}
+`
+// {
+//   "id": ["8229bff3-314d-40b6-aa11-6856f563123c"]
+// }
