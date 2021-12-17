574bc5d872ba4fb6689578a8ef8d12ce6bee4aab
mhoobler <hoobler.michael@gmail.com>
2021-12-16 22:57:50 -0500

    User page styling fixes

--- screens/Login/SignIn.js
+++ screens/Login/SignIn.js
@@ -4,6 +4,7 @@ import {
   Text,
   Heading,
   Button,
+  Pressable,
   Link,
   Input,
   Center,
@@ -44,20 +45,24 @@ const SignIn = ({ toSignUp, signinDispatch }) => {
 
   return (
     <Center flex="1" safeArea>
-      <VStack flex="5" justifyContent="flex-end">
-        <Text fontSize="3xl" textAlign="center">
+      <VStack flex="5" alignItems="center" justifyContent="flex-end">
+        <Text fontSize="5xl" textAlign="center">
           Welcome Back
         </Text>
-        <Flex maxH="64" maxW="64">
+        <Center maxH="64" maxW="64">
           <PresentsSvg />
-        </Flex>
-        <Center mb={4} mt="4">
-          <Text>Log In</Text>
-          <HStack>
-            <Text>or </Text>
-            <Link onPress={toSignUp}>create an account</Link>
-          </HStack>
         </Center>
+        <VStack my={8} justifyContent="center" alignItems="center">
+          <Text fontSize="2xl">Log In</Text>
+          <HStack>
+            <Text fontSize="2xl">or </Text>
+            <Pressable onPress={toSignUp}>
+              <Text fontSize="2xl" underline>
+                create an account
+              </Text>
+            </Pressable>
+          </HStack>
+        </VStack>
         {error && (
           <Center>
             <HStack>
@@ -67,15 +72,17 @@ const SignIn = ({ toSignUp, signinDispatch }) => {
         )}
       </VStack>
 
-      <VStack w="48" flex="5" space={4} justifyContent="flex-start">
-        <Flex h="8">
-          <Input {...username} placeholder="username" />
+      <VStack w="64" flex="5" space={2} justifyContent="flex-start">
+        <Flex h="10">
+          <Input {...username} placeholder="username" fontSize="md" />
         </Flex>
-        <Flex h="8">
-          <Input {...password} placeholder="password" />
+        <Flex h="10">
+          <Input {...password} placeholder="password" fontSize="md" />
         </Flex>
-        <Button isLoading={isLoading} onPress={handleSubmit}>
-          Sign In
+        <Button mt="4" isLoading={isLoading} onPress={handleSubmit}>
+          <Text fontSize="lg" color="white">
+            Sign In
+          </Text>
         </Button>
         <VStack alignItems="center">
           <Link>Forgot Your Password?</Link>
