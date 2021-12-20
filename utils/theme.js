8100974546c8aee272591a96f354fb4aa22a0612
CakeCrusher <1sebastian1sosa1@gmail.com>
2021-12-19 22:01:19 -0500

    app config

--- utils/theme.js
+++ utils/theme.js
@@ -1,4 +1,4 @@
-import { extendTheme } from 'native-base';
+import { extendTheme } from "native-base";
 import {
   useFonts,
   Source_Sans_Pro_ExtraLight200,
@@ -13,7 +13,7 @@ import {
   Source_Sans_Pro_Bold700_Italic,
   Source_Sans_Pro_Black900,
   Source_Sans_Pro_Black900_Italic,
-} from '@expo-google-fonts/source-sans-pro';
+} from "@expo-google-fonts/source-sans-pro";
 
 let [fontsLoaded] = useFonts({
   Source_Sans_Pro_ExtraLight200,
@@ -33,28 +33,28 @@ let [fontsLoaded] = useFonts({
 const theme = extendTheme({
   colors: {
     primary: {
-      50: '#e75472',
-      100: '#e54364',
-      200: '#e23054',
-      300: '#df1b43',
-      400: '#dc0430',
-      500: '#c9042c',
-      600: '#b40427',
-      700: '#a40423',
-      800: '#950420',
-      900: '#87041d',
+      50: "#e75472",
+      100: "#e54364",
+      200: "#e23054",
+      300: "#df1b43",
+      400: "#dc0430",
+      500: "#c9042c",
+      600: "#b40427",
+      700: "#a40423",
+      800: "#950420",
+      900: "#87041d",
     },
     secondary: {
-      50: '#f2aabb',
-      100: '#e592a2',
-      200: '#e28799',
-      300: '#df7b8f',
-      400: '#dc6e84',
-      500: '#d95f78',
-      600: '#c5566d',
-      700: '#b34e63',
-      800: '#a3475a',
-      900: '#944152',
+      50: "#f2aabb",
+      100: "#e592a2",
+      200: "#e28799",
+      300: "#df7b8f",
+      400: "#dc6e84",
+      500: "#d95f78",
+      600: "#c5566d",
+      700: "#b34e63",
+      800: "#a3475a",
+      900: "#944152",
     },
   },
   fontConfig: {},
@@ -69,50 +69,50 @@ const theme = extendTheme({
   fontConfig: {
     SansPro: {
       200: {
-        normal: 'Source_Sans_Pro_ExtraLight200',
-        italic: 'Source_Sans_Pro_ExtraLight200_Italic',
+        normal: "Source_Sans_Pro_ExtraLight200",
+        italic: "Source_Sans_Pro_ExtraLight200_Italic",
       },
       300: {
-        normal: 'Source_Sans_Pro_ExtraLight300',
-        italic: 'Source_Sans_Pro_ExtraLight300_Italic',
+        normal: "Source_Sans_Pro_ExtraLight300",
+        italic: "Source_Sans_Pro_ExtraLight300_Italic",
       },
       400: {
-        normal: 'Source_Sans_Pro_Regular400',
-        italic: 'Source_Sans_Pro_Regular400_Italic',
+        normal: "Source_Sans_Pro_Regular400",
+        italic: "Source_Sans_Pro_Regular400_Italic",
       },
     },
   },
   fonts: {
-    heading: 'Roboto',
-    body: 'Roboto',
-    mono: 'Roboto',
+    heading: "Roboto",
+    body: "Roboto",
+    mono: "Roboto",
   },
   components: {
     Button: {
       baseStyle: {
         _stack: { m: -1 },
-        rounded: 'full',
+        rounded: "full",
       },
       variants: {
         outline: {
-          borderWidth: '3',
+          borderWidth: "3",
         },
       },
       defaultProps: {
-        colorScheme: 'secondary',
+        colorScheme: "secondary",
       },
     },
     Input: {
       baseStyle: {
-        bg: '#FFFFFF',
-        color: '#3a3a3a',
+        bg: "#FFFFFF",
+        color: "#3a3a3a",
         flex: 1,
         maxH: 16,
       },
     },
     Text: {
       baseStyle: {
-        color: '#3a3a3a',
+        color: "#3a3a3a",
       },
     },
   },
